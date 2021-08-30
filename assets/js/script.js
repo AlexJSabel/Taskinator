8b053a71e63ef7c5ab1b98dbc9aa079ee81630ad
unknown <alexj.sabel@gmail.com>
2021-08-29 20:29:15 -0500

    added more html and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,11 @@
-
-var buttonEl = document.querySelector('#save-task');
+var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHander = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click" , createTaskHander);
-
-
-
+buttonEl.addEventListener("click", createTaskHandler);
